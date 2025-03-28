042d64a0f3d30b5b1837f21beb1f322feb3c2217
Marta Arevalo <martaarevaloprieto@gmail.com>
2025-03-28 16:06:25 +0100

    [Fita 3 - DONE] - crear el model Reserva amb fks i afegir dades via shell

--- lloguer/admin.py
+++ lloguer/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from .models import *
+from .models import Automobil, Reserva
 
-# Register your models here.
+# Registrar Automobil con su configuración personalizada
+class AutomobilAdmin(admin.ModelAdmin):
+    list_display = ('marca', 'model', 'matricula')  
+
+# Registrar Reserva con su configuración personalizada
+class ReservaAdmin(admin.ModelAdmin):
+    list_display = ('automobil', 'usuario', 'data_inici', 'data_fi')
+
 admin.site.register(Automobil)
+admin.site.register(Reserva)
--- lloguer/models.py
+++ lloguer/models.py
@@ -7,5 +7,35 @@ class Automobil(models.Model):
     model = models.CharField(max_length=50)
     matricula = models.CharField(max_length=10)
 
+    # mostrar en el admin panel correctamente los automobiles
     def __str__(self):
         return f"{self.marca} {self.model} - {self.matricula}"
+
+# Fita 3: crear modelo con FKs y añadir datos via shell
+class Reserva(models.Model):
+    automobil = models.ForeignKey(Automobil, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    data_inici = models.DateField()
+    data_fi = models.DateField()
+
+    class Meta:
+        unique_together = ('automobil', 'data_inici')
+
+    def __str__(self):
+        return f"Reserva de {self.automobil} per {self.usuario.username} des de {self.data_inici} fins {self.data_fi}"
+
+# - SHELL -
+# from lloguer.models import Automobil, Reserva
+# from django.contrib.auth.models import User
+
+# usuario = User.objects.get(username="admin")
+# automobil = Automobil.objects.get(matricula="6666 GGG")
+
+# from datetime import date
+
+# reserva = Reserva.objects.create(
+#     automobil=automobil,
+#     usuario=usuario,
+#     data_inici=date(2024, 4, 1),
+#     data_fi=date(2024, 4, 5)
+# )
